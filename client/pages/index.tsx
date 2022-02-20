1158f07d8f2bdeadaffcaeedf8256ec5cc6e7171
jawad halby makary <jawadhalbymakary@gmail.com>
2022-02-20 14:43:42 +0200

    added schemas

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Uniswap clone </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        hello, world
+      </div>
+  )
+}
+
+export default Home
