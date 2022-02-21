51a0640e519eb4b7b3a40d666d0ec0fdfefd3fea
jawad halby makary <jawadhalbymakary@gmail.com>
2022-02-21 20:09:38 +0200

    added smart contract for transfering crypto

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const transactionFactory = await hre.ethers.getContractFactory(
+    "Transactions"
+  );
+  const transactionContract = await transactionFactory.deploy();
+  await transactionContract.deployed();
+  console.log("Transactions deployed to: ", transactionContract.address);
+  //IIF --> IMMEDIATELY ENVOKED FUNCTION
+  (async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  })();
+};
